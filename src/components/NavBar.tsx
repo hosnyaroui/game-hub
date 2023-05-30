05f3d115e7c27a015a4735ceb7c5b1c788ed5148
hosnyaroui <hosny.cris@gmail.com>
2023-05-30 13:52:56 +0100

    Build a Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { HStack, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp';
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <HStack>
+        <Image src={logo} boxSize='60px'/>
+        <Text>NavBar</Text>
+    </HStack>
   )
 }
 
