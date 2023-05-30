3f716c22169daf11e90db88522c1f165fc07eedb
hosnyaroui <hosny.cris@gmail.com>
2023-05-30 14:51:14 +0100

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ return  <Grid templateAreas={
 <GridItem area='nav' ><NavBar /></GridItem>
 
 <Show above="lg">
-<GridItem area='aside' bg='gold'>Aside</GridItem>
+<GridItem area='aside' >Aside</GridItem>
 </Show>
-<GridItem area='main' bg='dodgerblue'>Main</GridItem>
+<GridItem area='main' >Main</GridItem>
 </Grid>
  
 }
