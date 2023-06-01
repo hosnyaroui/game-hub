5cb7b2644841453410b4d42e301e6c261e39755b
hosnyaroui <hosny.cris@gmail.com>
2023-06-01 08:08:58 +0100

    Hilight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,10 +5,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
 onSelectGenre: (genre: Genre) => void;
-
+selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre}: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre}: Props) => {
 const {data, isLoading, error} = useGenres();
 
 if (error) return null;
@@ -19,7 +19,7 @@ if (isLoading) return <Spinner />;
 {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
     <HStack>
         <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-        <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
+        <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
             {genre.name}
         </Button>
     </HStack>
